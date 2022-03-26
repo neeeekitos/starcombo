51fcdd65e96a0b6643cea21319afcf02832a52bc
msaug <msaug@protonmail.com>
2022-03-26 11:22:01 +0100

    finished work with sdk

--- components/Invocations.tsx
+++ components/Invocations.tsx
@@ -16,8 +16,20 @@ import {ChainId, Fetcher, Pair, Percent, Route, Token, TokenAmount, Trade} from 
 const getPoolInfo = async (poolNumber: string) => {
   const mySwapRouterContract = new Contract(mySwapRouter.abi as Abi, "0x071faa7d6c3ddb081395574c5a6904f4458ff648b66e2123b877555d9ae0260e");
   return await mySwapRouterContract.call("get_pool", [poolNumber]);
-
 }
+
+const getLiquidityPoolAddress = async (provider:Provider,tokenFrom:string,tokenTo:string) => {
+  const liquidityPoolForTokens = await provider.callContract({
+    contractAddress:JEDI_REGISTRY_ADDRESS,
+    entrypoint:"get_pair_for",
+    calldata:[
+      ethers.BigNumber.from(tokenFrom).toBigInt().toString(),
+      ethers.BigNumber.from(tokenTo).toBigInt().toString()
+    ]
+  }).then((res) => res.result[0])
+  return liquidityPoolForTokens;
+}
+
 const Invocations = () => {
 
   const [acc, setAcc] = useState<AccountInterface>();
@@ -113,6 +125,7 @@ const Invocations = () => {
 
   const jediSwap = async () => {
 
+    if(!provider || !acc) return;
     const amountFrom = "100000000000000000000";
     const amountTo = "3316"; //not necessary anymore for exact_tokens_for_tokens
     const tokenFrom = "0x04bc8ac16658025bff4a3bd0760e84fcf075417a4c55c6fae716efdd8f1ed26c"; //jedifeb0
@@ -134,20 +147,15 @@ const Invocations = () => {
     )
 
     //Fetches liq pool address for tokenA and tokenB
-    const liquidityPoolForTokens = await provider?.callContract({
-      contractAddress:JEDI_REGISTRY_ADDRESS,
-      entrypoint:"get_pair_for",
-      calldata:[
-        ethers.BigNumber.from(tokenFrom).toBigInt().toString(),
-        ethers.BigNumber.from(tokenTo).toBigInt().toString()
-      ]
-    }).then((res) => res.result[0])
+    const liquidityPoolForTokens = await getLiquidityPoolAddress(provider,tokenFrom,tokenTo);
+
 
     //TODO throw error for frontend if couldn't find pool
     if(!liquidityPoolForTokens) return;
     console.log(liquidityPoolForTokens)
 
-    const liqPoolToken0 =await provider?.callContract({
+    //Returns token0 address
+    const liqPoolToken0 =await provider.callContract({
       contractAddress:liquidityPoolForTokens!,
       entrypoint:"token0",
     }).then((res) => ethers.BigNumber.from(res.result[0]).toString())
@@ -156,7 +164,7 @@ const Invocations = () => {
     console.log(liqPoolToken0);
 
     // get reserves for the tokenA tokenB liq pool
-    const liqReserves =await provider?.callContract({
+    const liqReserves =await provider.callContract({
       contractAddress:liquidityPoolForTokens!,
       entrypoint:"get_reserves",
     }).then((res) => res.result);
@@ -180,10 +188,30 @@ const Invocations = () => {
     console.log(trade)
     console.log("execution price: $" + trade.executionPrice.toSignificant(6));
     console.log("price impact: " + trade.priceImpact.toSignificant(6) + "%");
+
+    //TODO user-chosen value here
     const slippageTolerance = new Percent('50', '10000'); // 0.5%
     const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw;
     const amountOutMinDec = ethers.BigNumber.from(amountOutMin.toString()).toBigInt()
     console.log(amountOutMinDec)
+
+    const path = trade.route.path;
+    const pathLength = path.length;
+    const pathAddresses = path.map((token) => ethers.BigNumber.from(token.address.toString()).toBigInt()).toString();
+
+    //flatten the calldata to make it into a single 1D array
+    const swapCallData = [
+      amountFrom,
+      "0",
+      amountOutMinDec.toString(),
+      "0",
+      pathLength.toString(),
+      pathAddresses,
+      ethers.BigNumber.from(receiver).toBigInt().toString(),
+      Math.floor((Date.now() / 1000) + 3600).toString() // default timeout is 1 hour
+    ].flatMap((x) => x);
+
+    console.log(swapCallData)
 
 
     console.log(`receiver: ${receiver}`);
@@ -203,17 +231,7 @@ const Invocations = () => {
       {
         contractAddress: JEDI_ROUTER_ADDRESS,
         entrypoint: 'swap_exact_tokens_for_tokens',
-        calldata: [
-          amountFrom,
-          "0",
-          amountOutMinDec.toString(),
-          "0",
-          "2",
-          ethers.BigNumber.from(tokenFrom).toBigInt().toString(),
-          ethers.BigNumber.from(tokenTo).toBigInt().toString(),
-          ethers.BigNumber.from(receiver).toBigInt().toString(),
-          Math.floor((Date.now() / 1000) + 3600).toString() // default timeout is 1 hour
-        ]
+        calldata: swapCallData
       }
     ];
     const result = await acc!.execute(tx);
