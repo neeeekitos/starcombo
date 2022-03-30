b39897f734d082381fa0fa4647aca02f91f4167c
msaug <msaug@protonmail.com>
2022-03-30 20:14:32 +0200

    bugfix: addLiquidity + myswap removeLiquidity

--- hooks/mySwap.ts
+++ hooks/mySwap.ts
@@ -3,7 +3,7 @@ import {ethers} from "ethers";
 
 import {Abi, Call, Contract, number} from "starknet";
 import mySwapRouter from "../contracts/artifacts/abis/myswap/router.json";
-import {ChainId, Pair, Percent, Token, TokenAmount, Trade} from "@jediswap/sdk";
+import {BigintIsh, ChainId, Fraction, Pair, Percent, Price, Token, TokenAmount, Trade} from "@jediswap/sdk";
 import {JEDI_ROUTER_ADDRESS, MY_SWAP_ROUTER_ADDRESS, SLIPPAGE} from "../utils/constants/constants";
 import {PoolPosition} from "./jediSwap";
 
@@ -56,10 +56,8 @@ export class MySwap implements DexCombo {
    */
   async findBestTrade(from: Token, to: Token, pairFromTo: Pair, amountFrom: string, amountTo: string, slippageTolerance: Percent): Promise<TradeInfo | undefined> {
     //Create poolPair to find the best trade for this poolPair. Use liq reserves as poolPair amounts
-    console.log(pairFromTo, from, amountFrom, to)
     let trade = Trade.bestTradeExactIn([pairFromTo], new TokenAmount(from, amountFrom), to)[0];
 
-    console.log(trade)
     console.log("execution price: $" + trade.executionPrice.toSignificant(6));
     console.log("price impact: " + trade.priceImpact.toSignificant(6) + "%");
 
@@ -113,67 +111,69 @@ export class MySwap implements DexCombo {
     //TODO check if it's ok if amtToken0 corresponds to pool token 1
     //TODO check if there's another way to add fixed amountToken1 ? This works only if amountTokenFrom refers to Token0.
 
-    //Add liquidity to pool with poolPair 0 and poolPair 1.
-    //We provide tokenAmountFrom:TokenAmount.
-    //so if token0 == tokenFrom we're gucci otherwise we must invert the values.
-    const tokenFromIsToken0 = tokenAmountFrom.token.address === poolPair.token0.address;
-    const tokenFromDec = number.toBN(poolPair.token0.address).toString();
-    const tokenToDec = number.toBN(poolPair.token1.address).toString();
-    const token0Dec = tokenFromIsToken0 ? tokenFromDec : tokenToDec;
-    const token1Dec = tokenFromIsToken0 ? tokenToDec : tokenFromDec
+    let tokenFrom, tokenTo, tokenFromIsToken0, tokenFromPrice: Price;
+    console.log(tokenAmountFrom.token.address, poolPair.token0.address)
+    tokenAmountFrom.token.address === poolPair.token0.address ?
+      [tokenFrom, tokenTo, tokenFromPrice, tokenFromIsToken0] = [poolPair.token0, poolPair.token1, poolPair.token0Price, true] :
+      [tokenFrom, tokenTo, tokenFromPrice, tokenFromIsToken0] = [poolPair.token1, poolPair.token0, poolPair.token1Price, false];
 
-    //get output amt for token input
-    let outputAmt, desiredAmount0, rawOutputAmt, minAmount0, desiredAmount1, minAmount1;
-    if (tokenFromIsToken0) {
-      desiredAmount0 = ethers.BigNumber.from(tokenAmountFrom.raw.toString());
-      minAmount0 = desiredAmount0.sub(slippage.multiply(desiredAmount0.toBigInt()).toFixed(0)).toString()
-      outputAmt = poolPair.getOutputAmount(new TokenAmount(poolPair.token0, tokenAmountFrom.raw.toString()));
-      desiredAmount1 = ethers.BigNumber.from(outputAmt[0].raw.toString());
-      minAmount1 = desiredAmount1.sub(slippage.multiply(desiredAmount1.toBigInt()).toFixed(0)).toString()
+    const tokenFromDec = number.toBN(tokenFrom.address);
+    const tokenToDec = number.toBN(tokenTo.address);
 
-    } else {
-      desiredAmount1 = ethers.BigNumber.from(tokenAmountFrom.raw.toString());
-      minAmount1 = desiredAmount1.sub(slippage.multiply(desiredAmount1.toBigInt()).toFixed(0)).toString()
-      outputAmt = poolPair.getOutputAmount(new TokenAmount(poolPair.token1, tokenAmountFrom.raw.toString()));
-      desiredAmount0 = ethers.BigNumber.from(outputAmt[0].raw.toString());
-      minAmount0 = desiredAmount0.sub(slippage.multiply(desiredAmount0.toBigInt()).toFixed(0)).toString()
-    }
+    let desiredAmountFrom: ethers.BigNumber, minAmountFrom: string, desiredAmountTo: Fraction, minAmountTo: string;
+
+
+    // from
+    desiredAmountFrom = ethers.BigNumber.from(tokenAmountFrom.raw.toString());
+    console.log(desiredAmountFrom)
+    minAmountFrom = desiredAmountFrom.sub(SLIPPAGE.multiply(desiredAmountFrom.toBigInt()).toFixed(0)).toString()
+
+    // to
+    desiredAmountTo = tokenFromPrice.raw.multiply(desiredAmountFrom.toString());
+    console.log(desiredAmountTo)
+    minAmountTo = desiredAmountTo.subtract(SLIPPAGE.multiply(desiredAmountTo).toFixed(0)).toFixed(0)
+
+    console.log(desiredAmountFrom.toString(), desiredAmountTo.toFixed(0), minAmountFrom, minAmountTo)
+    console.log(tokenFromIsToken0)
+
+    const callData: Array<string> = [
+        tokenFromIsToken0 ? tokenFromDec.toString() : tokenToDec.toString(),
+        tokenFromIsToken0 ? desiredAmountFrom.toString() : desiredAmountTo.toFixed(0),
+        "0",
+        tokenFromIsToken0 ? minAmountFrom : minAmountTo,
+        "0",
+        tokenFromIsToken0 ? tokenToDec.toString() : tokenFromDec.toString(),
+        tokenFromIsToken0 ? desiredAmountTo.toFixed(0) : desiredAmountFrom.toString(),
+        "0",
+        tokenFromIsToken0 ? minAmountTo : minAmountFrom,
+        "0"
+      ]
+    ;
 
 
     const tx = [
       {
-        contractAddress: poolPair.token0.address,
+        contractAddress: tokenFrom.address,
         entrypoint: 'approve',
         calldata: [
           number.toBN(MY_SWAP_ROUTER_ADDRESS).toString(), // router address decimal
-          desiredAmount0.toString(),
+          desiredAmountFrom.toBigInt().toString(),
           "0"
         ]
       },
       {
-        contractAddress: poolPair.token1.address,
+        contractAddress: tokenTo.address,
         entrypoint: 'approve',
         calldata: [
           number.toBN(MY_SWAP_ROUTER_ADDRESS).toString(), // router address decimal
-          desiredAmount1.toString(),
+          desiredAmountTo.toFixed(0),
           "0"
         ]
       },
       {
         contractAddress: MY_SWAP_ROUTER_ADDRESS,
         entrypoint: 'add_liquidity',
-        calldata: [
-          token0Dec,
-          desiredAmount0.toString(),
-          "0", //this is bcause desiredAmount is a uint256 with 2 members: low and high. we set high to 0.
-          minAmount0,
-          "0",
-          token1Dec,
-          desiredAmount1.toString(),
-          "0",
-          minAmount1,
-          "0"
-        ]
+        calldata: callData
       }
     ];
     return tx;
@@ -187,33 +187,30 @@ export class MySwap implements DexCombo {
 
   removeLiquidity(starknetConnector: StarknetConnector, poolPosition: PoolPosition, liqToRemove: TokenAmount): Call | Call[] {
 
-    const poolPair: Pair = poolPosition.poolPair;
-
-    console.log(poolPosition.poolSupply.raw.toString())
-    console.log(liqToRemove.raw.toString())
-    console.log(poolPair.reserve0.raw.toString())
-    console.log(poolPair.reserve1.raw.toString())
-    let token0Amount = poolPair.getLiquidityValue(poolPair.token0, poolPosition.poolSupply, liqToRemove);
-    let token1Amount = poolPair.getLiquidityValue(poolPair.token1, poolPosition.poolSupply, liqToRemove);
-    console.log(token0Amount.raw.toString())
-    console.log(token1Amount.raw.toString())
-
+    const poolPair = poolPosition.poolPair;
+    let poolShare = poolPosition.userLiquidity.divide(poolPosition.poolSupply); // represents the %of the pool the user owns.
+    //token0Amount is reserve0*poolShare
+    let token0Amount = poolPair.reserve0.multiply(poolShare);
+    let token1Amount = poolPair.reserve1.multiply(poolShare)
+    //These values are not in WEI but in unit... so we need to find a way to give the wei value without rounding.
+    //To do so we parse units the whole result with the token decimals.
+    // Inside this, we're calculating tokenAmount(1-slippage). Note that this result is in unit and not wei terms so we need to parseUnit all of this :)
+    let token0min = ethers.utils.parseUnits(token0Amount.subtract(token0Amount.multiply(SLIPPAGE)).toFixed(poolPair.token0.decimals), poolPair.token0.decimals);
+    let token1min = ethers.utils.parseUnits(token1Amount.subtract(token1Amount.multiply(SLIPPAGE)).toFixed(poolPair.token1.decimals), poolPair.token1.decimals);
     const tx = {
       contractAddress: MY_SWAP_ROUTER_ADDRESS,
       entrypoint: "withdraw_liquidity",
       calldata: [
-        poolPosition.poolPair.pairAddress,
+        poolPosition.poolPair.pairAddress, //no need for BN ops here because it's just the poolId
         liqToRemove.raw.toString(),
         "0",
-        token0Amount.raw.toString(),
+        token0min.toString(),
         "0",
-        token1Amount.raw.toString(),
+        token1min.toString(),
         "0"
       ]
     }
     return tx;
-
-
   }
 
   revoke(): void {
@@ -230,7 +227,7 @@ export class MySwap implements DexCombo {
       entrypoint: "get_lp_balance",
       calldata: [
         poolId,
-        ethers.BigNumber.from(starknetConnector.account.address).toBigInt().toString(),
+        number.toBN(starknetConnector.account.address).toString()
       ]
     }).then((res) => res.result[0]);
 
